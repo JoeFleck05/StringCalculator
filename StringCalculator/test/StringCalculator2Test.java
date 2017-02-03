43977ba54ff4153270fc21acc20c931e1aebad2f
Joseph Fleck <josephfleck@josephs-mbp.attlocal.net>
2017-02-02 22:03:56 -0500

    Next Attempt

---
+++ StringCalculator/test/StringCalculator2Test.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author josephfleck
+ */
+public class StringCalculator2Test {
+    
+    public StringCalculator2Test() {
+    }
+    
+}
